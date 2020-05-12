dfe484332ff273254c148c6b7cbba2c54cb63781
maxmartinezruts <38012876+maxmartinezruts@users.noreply.github.com>
2020-05-12 22:19:05 +0200

    Update parameters.py

--- simulation/parameters.py
+++ simulation/parameters.py
@@ -1,5 +1,5 @@
 
 #                       kg              J/kg                 Watts          m/s
 concept_A =  dict(battery_mass=3, energy_density=500000, pow_cruise=500, vel_cruise=80)
-concept_B =  dict(battery_mass=1, energy_density=500000, pow_cruise=400, vel_cruise=80)
+concept_B =  dict(battery_mass=2, energy_density=500000, pow_cruise=400, vel_cruise=80)
 concept_C =  dict(battery_mass=2, energy_density=500000, pow_cruise=500, vel_cruise=80)
