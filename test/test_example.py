f5c87d88a6f89de5e5ae0e622633d238e410c957
maxmartinezruts <38012876+maxmartinezruts@users.noreply.github.com>
2020-05-12 21:04:30 +0200

    Update test_example.py

--- test/test_example.py
+++ test/test_example.py
@@ -1,13 +1,3 @@
-# import sys
-# sys.path.append('..')
 
-# from tutorial_VV.parameters import concept_A, concept_B, concept_C
-# from tutorial_VV.model import get_range
-#
-# def test_ranges():
-#
-#     assert get_range(concept_A) > 150000, 'Range of concept A is insufficient'
-#     assert get_range(concept_B) > 150000, 'Range of concept B is insufficient'
-#     assert get_range(concept_C) > 1500000, 'Range of concept C is insufficient'
 def hello_world():
     pass
