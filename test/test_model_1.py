956b2f4811007a66417a35b4acc7c92d9b4ec425
maxmartinezruts <38012876+maxmartinezruts@users.noreply.github.com>
2020-05-13 07:33:23 +0200

    Update test_model_1.py

--- test/test_model_1.py
+++ test/test_model_1.py
@@ -9,5 +9,4 @@ def test_ranges():
     assert get_range(concept_A) > 150000, 'Range of concept A is insufficient'
     assert get_range(concept_B) > 150000, 'Range of concept B is insufficient'
     assert get_range(concept_C) > 150000, 'Range of concept C is insufficient'
-def hello_world():
-    pass
+
